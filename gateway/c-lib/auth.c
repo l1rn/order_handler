b8501b02b83023fdea3134e9d77bab8528c1572b
l1rn <ivtaks06@gmail.com>
2026-02-12 11:22:10 +0500

    openssl / base64

--- gateway/c-lib/auth.c
+++ gateway/c-lib/auth.c
@@ -1,6 +1,30 @@
 #include <string.h>
 #include <jwt.h>
 
+int base64_decode(const char *input, unsigned char **output){
+	size_t len = strlen(input);
+	size_t padding = 0;
+	if (input[len-1] == '=') padding++;
+	if (input[len-2] == '=') padding++;
+	
+	size_t out_len = (len * 3) / 4 - padding;
+	*output = malloc(out_len + 1);
+
+	BIO *bio, b64;
+	b64 = BIO_NEW(BIO_f_base64());
+	bio = BIO_new_mem_buf(input, -1);
+	bio = BIO_push(b64, bio);
+	BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
+	BIO_read(bio, *output, len);
+	BIO_free_all(bio);
+
+	return out_len;
+}
+
+void base64url_decode_to_str(const char *input, char *output){
+	char temp[10240];
+}
+
 int check_token(const char *t){
 	if(strncmp(t, "Bearer ", 7) != 0){
 		return 0;
